0db4230129ea093baec67486f5f0c93010eef3e4
KristhelHG <153631358+KristhelHG@users.noreply.github.com>
2023-12-12 09:54:56 -0500

    Add files via upload

---
+++ Primer programa PAE.py
@@ -0,0 +1,53 @@
+#EJERCICIO 1
+#Este programa realiza operaciones básicas y muestra la manipulación de variables en Python.
+
+#Se procedera a realizar las siguientes operaciones:
+
+suma = 3 + 2
+resta = 7.4 - 1.3
+multiplicacion = 12 * 2
+division = 77.5 / 25
+print (suma, resta, multiplicacion, division)
+
+#En la segunda linea se realizo una suma con dos numeros enteros siendo estos 3 y 2
+#En la tercera linea se realizo una resta de dos numeros flotantes siendo estos 7.4 y 1.3
+#En la cuarta linea se realizo una multiplicacion con los valores 12 y 2
+#Por ultimo se realizo una division con dos numeros flotantes con los valores 77.5 y 25
+
+########################################################################################################
+
+#EJERCICI0 2
+#Este programa tiene como objetivo demostrar el uso de funciones de manipulación de cadenas de texto en Python.
+
+nombre = "Kristhel Romina Heredia Godoy"
+longitud = len(nombre)
+print('La longitud del nombre',nombre,'es',longitud)
+
+#Comienza declarando una variable nombre que en este caso es mi nombre completo.
+#Luego se usa la funcion len() para mostrar la longitud de el nombre y la guarda como una variable.
+#Por ultimo la informacion resultante se imprime en la pantalla mostrando el nombre completo y la longitud.
+#Gracias esto se puede entender de una mejor manera como es trabajar con la funcion len() y las cadenas de texto.
+
+########################################################################################################
+
+#EJERCICIO3
+#Este programa demuestra la manipulación y conversión de tipos de datos en Python
+
+entero = 10
+flotante = 4.15
+conversion_1 = float(entero)
+conversion_2 = int(flotante)
+print(entero, conversion_1)
+print(flotante, conversion_2)
+
+#El proposito del programa es enseñar el proceso de conversión de tipos de datos en Python.
+#Se incia con una variable entera y otra de punto flotante.
+#Luego se realiza la conversión de entero a punto flotante y viceversa.
+#Se utilizan las funciones int() y float() para las conversiones que se solicitan
+#Finalmente, se imprime en pantalla la variable original y la variable después de cada conversión.
+#Este código sirve para entender cómo Python maneja las conversiones entre tipos de datos numéricos.
+
+########################################################################################################
+
+
+
